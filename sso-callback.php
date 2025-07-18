f40617d45aff2e5fd4d281e2b47e38d59758da51
Arthur C. <63045074+arthurcorso@users.noreply.github.com>
2025-07-18 13:58:02 +0200

    Update sso-callback.php

--- sso-callback.php
+++ sso-callback.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
 
-$issuer = 'ton issuer url';
-$client_id = 'le client id';
-$client_secret = 'ton client secret';
-$redirect_uri = 'https://ton-whmcs.com/sso-callback.php';
-$adminUsername = "pseudo d'un compte admin whmcs pour les action"; // cree un new du type sso
+include 'sso-config.php';
 
 if (!isset($_GET['code'])) {
     die('No code provided');
